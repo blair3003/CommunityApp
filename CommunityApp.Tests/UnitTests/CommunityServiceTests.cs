70a4b7c2658208bba9d8d0f8547c2aec011bc720
Blair Fleming <blair3003.bf@gmail.com>
2024-05-26 13:12:40 +0100

    CommunityService init
    + CommunityServiceTests

---
+++ CommunityApp.Tests/UnitTests/CommunityServiceTests.cs
@@ -0,0 +1,39 @@
+﻿using CommunityApp.Data.Models;
+using CommunityApp.Data.Repositories.Interfaces;
+using CommunityApp.Services;
+using Moq;
+
+namespace CommunityApp.Tests.UnitTests
+{
+    public class CommunityServiceTests
+    {
+        [Fact]
+        public async Task GetAllCommunitiesAsync_ReturnsAllCommunities()
+        {
+            var mockRepository = new Mock<ICommunityRepository>();
+
+            var communities = new List<Community>
+            {
+                new() { Id = 1, Name = "Community 1" },
+                new() { Id = 2, Name = "Community 2" },
+                new() { Id = 3, Name = "Community 3" }
+            };
+
+            mockRepository
+                .Setup(repo => repo.GetAllAsync())
+                .ReturnsAsync(communities);
+
+            var communityService = new CommunityService(mockRepository.Object);
+
+            var result = await communityService.GetAllCommunitiesAsync();
+
+            Assert.NotNull(result);
+            Assert.Equal(3, result.Count);
+            Assert.Contains(result, c => c.Name == "Community 1");
+            Assert.Contains(result, c => c.Name == "Community 2");
+            Assert.Contains(result, c => c.Name == "Community 3");
+
+            mockRepository.Verify(repo => repo.GetAllAsync(), Times.Once);
+        }
+    }
+}
---
+++ CommunityApp/Services/CommunityService.cs
@@ -0,0 +1,17 @@
+﻿using CommunityApp.Data.Models;
+using CommunityApp.Data.Repositories.Interfaces;
+using System.Net.Sockets;
+
+namespace CommunityApp.Services
+{
+    public class CommunityService(ICommunityRepository repository)
+    {
+        ICommunityRepository _repository = repository;
+
+        public async Task<List<Community>> GetAllCommunitiesAsync()
+        {
+            var allCommunities = await _repository.GetAllAsync();
+            return allCommunities;
+        }
+    }
+}
